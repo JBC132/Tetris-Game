ed165cab105d0a4b58089fe5a0e23d6c084d6dd2
이진영 <jakerhee321@gmail.com>
2022-08-07 23:42:27 +0900

    Functions

---
+++ Python_Tetris.py
@@ -0,0 +1,56 @@
+import pygame
+import random
+
+pygame.font.init()
+
+s_width = 800
+s_height = 700
+play_width = 300
+play_height = 600
+block_size = 30
+
+top_left_x = (s_width - play_width) // 2
+top_left_y = s_height - play_height
+
+
+
+class Piece(object):
+    pass
+
+def create_gris(locked_pos = []):
+    pass
+
+def convert_shape_format():
+    pass
+
+def valid_space():
+    pass
+
+def check_lost():
+    pass
+
+def get_shape():
+    pass
+
+def draw_text_middle():
+    pass
+
+def draw_grid():
+    pass
+
+def clear_rows():
+    pass
+
+def draw_next_shape():
+    pass
+
+def draw_window():
+    pass
+
+def main():
+    pass
+
+def main_menu():
+    pass
+
+main_menu()
