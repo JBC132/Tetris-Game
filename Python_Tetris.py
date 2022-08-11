d7281bf803a0e6844207ad101a205dff1f76674b
이진영 <jakerhee321@gmail.com>
2022-08-11 23:12:29 +0900

    clear_rows function

--- Python_Tetris.py
+++ Python_Tetris.py
@@ -191,8 +191,26 @@ def draw_grid(surface, grid):
 
 
 
-def clear_rows():
-    pass
+def clear_rows(grid, locked):   # delete the full row --> create new row --> push all row above down
+    inc = 0
+    for i in range(len(grid)-1, -1, -1):
+        row = grid[i]
+        if (0,0,0) not in row:
+            inc += 1
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y+inc)
+                locked[newKey] = locked.pop(key)
+
 
 def draw_next_shape(shape, surface):
     font = pygame.font.SysFont('comicsans', 30)
@@ -292,6 +310,7 @@ def main(win):
             current_piece = next_piece
             next_piece = get_shape()
             change_piece = False
+            clear_rows(grid, locked_positions)
         
         draw_window(win, grid)
         draw_next_shape(next_piece, win)
