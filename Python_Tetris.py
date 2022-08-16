49ab3a56d187e7654a8ac34405bb81384cb86df2
이진영 <jakerhee321@gmail.com>
2022-08-16 18:22:55 +0900

    Finish Game

--- Python_Tetris.py
+++ Python_Tetris.py
@@ -233,9 +233,7 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy - 30))
 
 def update_score(nscore):
-    with open('scores.txt', 'r') as f:
-        lines = f.readlines()
-        score = lines[0].strip()
+    score = max_score()
 
     with open('scores.txt', 'w') as f:
         if int(score) > nscore:
@@ -243,8 +241,14 @@ def update_score(nscore):
         else:
             f.write(str(nscore))
 
+def max_score():
+    with open('score.txt', 'r') as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+    
+    return score
 
-def draw_window(surface, grid, score=0):
+def draw_window(surface, grid, score=0, last_score=0):
     surface.fill((0,0,0))
 
     pygame.font.init()
@@ -253,11 +257,20 @@ def draw_window(surface, grid, score=0):
     
     surface.blit(label, (top_left_x + play_width/2 - (label.get_width()/2), 30))
 
+    # current score
     font = pygame.font.SysFont('comicsans', 30)
     label = font.render('Score: ' + str(score), 1, (255,255,255))
 
     sx = top_left_x + play_width + 50
     sy = top_left_y + play_height/2 - 100
+
+    surface.blit(label, (sx + 30, sy + 160))
+
+    # last score
+    label = font.render('High Score: ' + last_score, 1, (255,255,255))
+
+    sx = top_left_x - 200
+    sy = top_left_y + 200
 
     surface.blit(label, (sx + 30, sy + 160))
 
@@ -272,6 +285,7 @@ def draw_window(surface, grid, score=0):
     # pygame.display.update()
 
 def main(win):
+    last_score = max_score()
     locked_positions = {}
     grid = create_grid(locked_positions)
 
@@ -344,7 +358,7 @@ def main(win):
             change_piece = False
             score += clear_rows(grid, locked_positions) * 10
         
-        draw_window(win, grid, score)
+        draw_window(win, grid, score, last_score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
         
